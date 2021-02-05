02f7c3b5be9ff9f5f87a9f7cd8cdb40aeb6a3a35
bichenxi <chenxi.b@mofadeng.com>
2021-02-05 15:08:57 +0800

    feat: 增加iconFont组件

---
+++ src/components/IconFont.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        123
+        <svg class="icon" aria-hidden="true">
+            <use :xlink:href="`#${name}`"></use>
+        </svg>
+        456
+    </div>
+</template>
+
+<script>
+// Doc: https://www.iconfont.cn/help/detail?helptype=code
+
+export default {
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+    },
+}
+</script>
+
+<style scoped>
+.icon {
+    display: inline-block;
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,21 @@
 <template>
     <div class="home">
         <head-view />
+        <div class="w-64 h-64 bg-gray-100">
+            <icon-font name="icon-x" />
+        </div>
     </div>
 </template>
 <script>
 import headView from '../components/Header'
+import iconFont from '../components/IconFont'
 
 export default {
     name: 'Home',
 
     components: {
         headView,
+        iconFont,
     },
 }
 </script>
