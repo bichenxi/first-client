8834d98ea49665e93d4cd7f50e1cf00a60bc4157
bichenxi <chenxi.b@mofadeng.com>
2021-02-20 16:30:33 +0800

    feat: 完善个人中心’

--- src/layouts/Customer.vue
+++ src/layouts/Customer.vue
@@ -1,12 +1,101 @@
 <template>
-    <div class="costomer">
-        <header class="w-100 bg-gray-900 p-3 text-white flex justify-center">
-            <div><router-link to="/"> 返回首页 </router-link></div>
-            <h3>个人中心</h3>
+    <div class="costomer bg-gray-100">
+        <header class="w-100 bg-gray-900 p-3 text-white flex items-center">
+            <h3 class="flex-1 text-center">个人中心</h3>
+            <div>
+                <router-link class="flex-1 hover:text-gray-300" to="/">
+                    返回首页
+                </router-link>
+            </div>
         </header>
-        <button @click="signOut" class="text-sm hover:text-gray-300">
-            退出登录
-        </button>
+        <div class="py-32 flex justify-center">
+            <div class="p-10 w-1/2 bg-white border border-gray-200">
+                <div>
+                    <h2 class="text-4xl pb-4 border-b border-gray-300">
+                        个人资料
+                    </h2>
+                    <div class="py-4 border-b border-gray-300">
+                        <div
+                            class="h-40 w-40 rounded-full bg-red-500 text-3xl flex justify-center items-center"
+                        >
+                            <div>{{ user.name }}</div>
+                        </div>
+                    </div>
+                    <div
+                        class="py-5 border-b border-gray-300 flex justify-between items-center"
+                    >
+                        <div class="flex-1 flex">
+                            <span>用户名</span>
+                            <input
+                                type="text"
+                                :value="user.name"
+                                class="flex-1 px-6"
+                            />
+                        </div>
+                        <div class="text-blue-500">
+                            <i class="el-icon-edit mr-4 text-2xl"></i>
+                            <span>修改</span>
+                        </div>
+                    </div>
+                    <div
+                        class="py-5 border-b border-gray-300 flex justify-between items-center"
+                    >
+                        <div>
+                            <span>邮箱</span>
+                            <input type="text" />
+                        </div>
+                        <div class="text-blue-500">
+                            <i class="el-icon-edit mr-4 text-2xl"></i>
+                            <span>修改</span>
+                        </div>
+                    </div>
+                    <div
+                        class="py-5 border-b border-gray-300 flex justify-between items-center"
+                    >
+                        <div>
+                            <span>用户名</span>
+                            <input type="text" class="pl-6" />
+                        </div>
+                        <div class="text-blue-500">
+                            <i class="el-icon-edit mr-4 text-2xl"></i>
+                            <span>修改</span>
+                        </div>
+                    </div>
+                    <div
+                        class="py-5 border-b border-gray-300 flex justify-between items-center"
+                    >
+                        <div>
+                            <span>联系地址</span>
+                            <input type="text" />
+                        </div>
+                        <div class="text-blue-500">
+                            <i class="el-icon-edit mr-4 text-2xl"></i>
+                            <span>修改</span>
+                        </div>
+                    </div>
+                    <div
+                        class="py-5 border-b border-gray-300 flex justify-between items-center"
+                    >
+                        <div>
+                            <span>个人介绍</span>
+                            <input type="text" />
+                        </div>
+                        <div class="text-blue-500">
+                            <i class="el-icon-edit mr-4 text-2xl"></i>
+                            <span>修改</span>
+                        </div>
+                    </div>
+                    <div class="pt-8 flex justify-center">
+                        <button
+                            @click="signOut"
+                            class="text-sm hover:text-gray-200 bg-red-600 py-2 px-4 text-white"
+                        >
+                            退出登录
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -27,3 +116,12 @@ export default {
     },
 }
 </script>
+
+<style scoped>
+input {
+    outline: none;
+}
+input:focus {
+    border: none;
+}
+</style>
